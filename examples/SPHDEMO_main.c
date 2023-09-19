cccb69dd1bd75db698f8ba62b204fe33495ef3af
Chris G <chris@saturn.earth>
2023-09-19 09:14:07 -0500

    After fixing a bug in the color processing, I was able to use 16 planes (or 0) as an argument to SRGP_begin().

--- examples/SPHDEMO_main.c
+++ examples/SPHDEMO_main.c
@@ -195,11 +195,8 @@ InitSPHIGS (int argc, char **argv)
 	 READ_FROM_STDIN = TRUE;
    
    /* STARTUP SPHIGS */
-#if 0
    numplanes = 0; numshades = 10;
-#endif
-   /* Stay with 8 planes. */
-   numplanes = 8; numshades = 10;
+
    if (READ_FROM_STDIN)
    {
       printf ("\nPlease enter number of framebuffer planes to allocate:\n");
